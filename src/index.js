3b63049fccf8c588b1601bbecc1d4eabb79fc9d8
s.chiba <s.chiba@ebacorp.jp>
2024-09-13 14:34:37 +0900

    React18の仕様に変更

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; // Import from 'react-dom/client' for React 18
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
+// Create a root container and use createRoot instead of ReactDOM.render
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
